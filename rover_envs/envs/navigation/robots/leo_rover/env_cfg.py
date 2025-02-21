682a48593d04165274f0546d30180a40c4e82d81
Blueguardian <tssa20@student.aau.dk>
2025-02-21 10:11:49 +0100

    Added configurations for new assets

---
+++ rover_envs/envs/navigation/robots/leo_rover/env_cfg.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from isaaclab.utils import configclass
+
+import rover_envs.mdp as mdp
+from rover_envs.assets.robots.leo_rover import LEO_ROVER_CFG
+from rover_envs.envs.navigation.rover_env_cfg import RoverEnvCfg
+
+
+@configclass
+class AAURoverEnvCfg(RoverEnvCfg):
+    """Configuration for the Leo rover environment."""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Define robot
+        self.scene.robot = LEO_ROVER_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+        # Define parameters for the Ackermann kinematics.
+        self.actions.actions = mdp.SkidSteeringSimpleCfg(
+            asset_name="robot",
+            wheelbase_length=0.37,
+            wheel_radius=0.0635,
+            drive_joint_names=[".*Drive_Joint"],
+            # offset=-0.0135
+        )
