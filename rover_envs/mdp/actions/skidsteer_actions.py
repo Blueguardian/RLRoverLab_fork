e92374a126ed0cc78a2c0e480c9d0bcb63856fa1
Blueguardian <tssa20@student.aau.dk>
2025-05-24 12:32:17 +0200

    Update to parameters and skidsteering control

--- rover_envs/mdp/actions/skidsteer_actions.py
+++ rover_envs/mdp/actions/skidsteer_actions.py
@@ -31,7 +31,7 @@ class SkidSteerAction(ActionTerm):
     _scale: torch.Tensor
     _offset: torch.Tensor
 
-    
+
 
     def __init__(self, cfg: actions_cfg.SkidSteeringSimpleCfg, env: ManagerBasedEnv):
         super().__init__(cfg, env)
@@ -90,20 +90,13 @@ class SkidSteerAction(ActionTerm):
 
 def skid_steer_simple(vx, omega, cfg, device):
     """Compute skid-steering wheel velocities."""
-    #
-    # vx[:][:] = 0
-    # omega[:][:] = -2
 
     # Instance configuration variables
     track_width = cfg.track_width  # Track width (m)
     wheel_r = cfg.wheel_radius  # Wheel radius (m)
 
-    lin_vel = torch.abs(vx)
-
-    vel_left = vx - (omega * track_width / 2) / wheel_r *2
-    vel_right = vx + (omega * track_width / 2) / wheel_r *2
+    vel_left = (vx - omega * track_width / 2) / wheel_r
+    vel_right = (vx + omega * track_width / 2) / wheel_r
 
     wheel_vel = torch.stack([vel_left, vel_right, vel_left, vel_right], dim=1)  # Order: FL, RL, FL, RR -> Leo rover
-    #wheel_vel = torch.stack([vel_left, vel_left, vel_right, vel_right], dim=1) # Order FL, FR, RL, RR -> Summit
-
     return wheel_vel
